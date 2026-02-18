7b52932389dc1869d44bf31bab36a1825c869ba2
tanzid-48 <mdtanzid.525@gmail.com>
2026-02-18 23:53:00 +0600

    Add cash out functionality with balance update logic

--- scritp/cashout.js
+++ scritp/cashout.js
@@ -1,11 +1,53 @@
 
-document.getElementById('cashOut-btn').addEventListener('click',function(){
- console.log("taka nai");
+document.getElementById('cashOut-btn').addEventListener('click', function () {
+    // 1.get the agent number
+    const cashOutNumberInput = document.getElementById('cashOut-number');
+    const cashOutNumber = cashOutNumberInput.value;
+    console.log(cashOutNumber);
+    // validation
+    if(cashOutNumber.length != 11){
+        alert("invalid Agent Number");
+        return;
+    }
+    
+    // 2.get the amount
+
+    let cashOutAmountInput = document.getElementById('cashOut-amount');
+    let cashOutAmount = cashOutAmountInput.value
+  console.log(cashOutAmount)
+ //3.get the balance    
+  let balanceElement = document.getElementById('balance')
+  let balance = balanceElement.innerText;
+  console.log(balance);
+
+  //4. new balance calculate
+  
+  let newBalance = Number(balance) - Number(cashOutAmount);
+
+    //  5. valid the balance
+
+    if(newBalance <0){
+        alert("Invalid Amount");
+        return;
+    }
+ 
+ //6. get the pin and verify
+ 
+ let cashOutPinInput = document.getElementById('cashOut-pin');
+ let pin = cashOutPinInput.value;
+  if(pin==='1234'){
+    // show an alert set new balance
+    alert("cashOut Successful")
+    balanceElement.innerText = newBalance;
+
+
+  }else{
+    // 5.show error alert
+    alert("Invalid Pin");
+    return;
+  }
 
 
 
 
-
-
-
-})
+});
