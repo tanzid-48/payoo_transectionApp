d26042363c571bfcb571f2cd6e20da8e39638769
tanzid-48 <mdtanzid.525@gmail.com>
2026-02-18 13:17:08 +0600

    feat(auth): implement basic login validation with redirect

--- scritp/login.js
+++ scritp/login.js
@@ -1,1 +1,26 @@
-console.log('login')
+document.getElementById('btn-login').addEventListener('click',   function (event) {
+//  1.get the mobile number
+   let inputNumber = document.getElementById('input-number')
+    let contactNumber = inputNumber.value;
+    console.log(contactNumber);
+
+    // 2. get the pin
+   let inputPin = document.getElementById('input-pin') 
+   let pin = inputPin.value;
+   console.log(pin);
+ 
+ //    3.match the pin and number
+  if(contactNumber === "01798546510" && pin ==="1234"){
+   
+    // 3.1 login home page
+    alert("Login Success");
+    window.location.assign('home.html')
+
+  } else{
+    alert("login Failed");
+    return;
+  }
+
+
+
+    })
